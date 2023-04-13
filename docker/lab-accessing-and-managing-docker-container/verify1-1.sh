8541c0ba14962b1279e5fb08adbc2a9ac44224a5
xiachengfan <xiachengfan@rockontrol.com>
2023-04-13 20:06:39 +0800

    update

--- docker/lab-accessing-and-managing-docker-container/verify1-1.sh
+++ docker/lab-accessing-and-managing-docker-container/verify1-1.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Check if the my-container container start.
 docker ps -a | grep nginx | grep my-container | grep Up
-# Check if the my-container log has been viewed
-history | grep "docker logs my-container"
