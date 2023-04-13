a91d328d47743eaa0f42aa726ab21df9646776eb
xiachengfan <xiachengfan@rockontrol.com>
2023-04-13 20:32:13 +0800

    update verify1-1

--- docker/lab-accessing-and-managing-docker-container/verify1-2.sh
+++ docker/lab-accessing-and-managing-docker-container/verify1-2.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Check if the my-container log has been viewed
-if history |grep docker | grep my-container | grep  -Eq logs; then
-  exit 0
-else
-  exit 1
-fi
+history |grep docker | grep my-container | grep  -Eq logs
