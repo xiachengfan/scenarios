2ea28d1e3851d96ca54abaeb334112a07100a804
xiachengfan <xiachengfan@rockontrol.com>
2023-04-13 20:16:33 +0800

    history vweify1-2.sh

--- docker/lab-accessing-and-managing-docker-container/verify1-2.sh
+++ docker/lab-accessing-and-managing-docker-container/verify1-2.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Check if the my-container log has been viewed
-if history | grep -Eq 'docker logs my-container'; then
-  exit 0
-else
-  exit 1
-fi
+history | grep -Eq 'docker logs my-container'
