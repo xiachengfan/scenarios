8f4bffa5c0b292a6e3f98f86f0183bacc69062f0
xiachengfan <xiachengfan@rockontrol.com>
2023-04-18 20:57:27 +0800

    modified:   kubernetes/lab-kubernetes-exec-command/step1.md 	modified:   kubernetes/lab-kubernetes-exec-command/verify1-2.sh

--- kubernetes/lab-kubernetes-exec-command/verify1-2.sh
+++ kubernetes/lab-kubernetes-exec-command/verify1-2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat ~/.zsh_history |grep kubectl | grep apply 
+cat ~/.zsh_history |grep kubectl | grep create
 cat ~/.zsh_history |grep kubectl | grep pod | grep nginx |grep v | grep  -Eq exec
