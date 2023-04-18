1fbcf3348a99a79b16da30f1a1ca6ece82696dde
xiachengfan <xiachengfan@rockontrol.com>
2023-04-18 16:45:08 +0800

    modified:   kubernetes/lab-kubernetes-exec-command/verify4-2.sh

--- kubernetes/lab-kubernetes-exec-command/verify4-2.sh
+++ kubernetes/lab-kubernetes-exec-command/verify4-2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 minikube kubectl -- get describe pod nginx-env |grep MY_VAR |grep my-value
-cat ~/.zsh_history |grep kubectl | grep pod | grep nginx-env| grep c | grep  -Eq exec
+cat ~/.zsh_history |grep kubectl | grep nginx-env| grep c | grep  -Eq exec
