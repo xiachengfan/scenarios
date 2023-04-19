7e0636bb1db65dd3e29dec3618cd967b32ec91c8
xiachengfan <xiachengfan@rockontrol.com>
2023-04-19 22:04:25 +0800

    modified:   kubernetes/lab-kubernetes-expose-command/verify2-2.sh

--- kubernetes/lab-kubernetes-expose-command/verify2-2.sh
+++ kubernetes/lab-kubernetes-expose-command/verify2-2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 NODE_PORT=$(minikube kubectl -- get svc hello-service -o jsonpath='{.spec.ports[0].nodePort}')
-cat ~/.zsh_history |grep curl |grep $NODE_PORT
+NODE_IP=$(minikube kubectl -- get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')
+cat ~/.zsh_history |grep curl |grep $NODE_PORT | grep $NODE_IP
