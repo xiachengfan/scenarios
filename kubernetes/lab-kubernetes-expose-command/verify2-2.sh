6fb08e1e48880ef6843ad6d0cb07395434d0681e
xiachengfan <xiachengfan@rockontrol.com>
2023-04-19 21:55:24 +0800

    modified:   kubernetes/lab-kubernetes-expose-command/index.json 	modified:   kubernetes/lab-kubernetes-expose-command/verify2-2.sh

--- kubernetes/lab-kubernetes-expose-command/verify2-2.sh
+++ kubernetes/lab-kubernetes-expose-command/verify2-2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-NODE_PORT=$(kubectl get svc hello-service -o jsonpath='{.spec.ports[0].nodePort}')
-
+NODE_PORT=$(minikube kubectl -- get svc hello-service -o jsonpath='{.spec.ports[0].nodePort}')
 cat ~/.zsh_history |grep curl |grep $NODE_PORT
