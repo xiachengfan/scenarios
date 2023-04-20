f53358ea7d4c0bc64f3755f9a250dfac2b9c8dc0
xiachengfan <xiachengfan@rockontrol.com>
2023-04-20 19:27:33 +0800

    modified:   kubernetes/lab-configuring-apps-with-configmaps/verify2-1.sh 	modified:   kubernetes/lab-configuring-apps-with-configmaps/verify3-2.sh 	modified:   kubernetes/lab-configuring-apps-with-configmaps/verify4-2.sh

--- kubernetes/lab-configuring-apps-with-configmaps/verify3-2.sh
+++ kubernetes/lab-configuring-apps-with-configmaps/verify3-2.sh
@@ -1,3 +1,3 @@
-!/bin/bash
+#!/bin/bash
 
 cat ~/.zsh_history | grep kubectl |grep exec |grep 'it' |grep 'sh'
--- kubernetes/lab-configuring-apps-with-configmaps/verify4-2.sh
+++ kubernetes/lab-configuring-apps-with-configmaps/verify4-2.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 data=$(minikube kubectl -- get configmap my-configmap -o jsonpath='{.data}')
 
