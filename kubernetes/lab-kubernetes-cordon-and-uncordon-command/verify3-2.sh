8fd68d2961c6953dded3785d256a214f54d5e0da
xiachengfan <xiachengfan@rockontrol.com>
2023-04-20 22:45:53 +0800

    modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/index.json 	modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-2.sh 	modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh 	modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-4.sh 	new file:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-5.sh 	modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify4-1.sh

--- kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-2.sh
+++ kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-2.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# minikube kubectl -- describe nodes | grep -E 'Unschedulable:[[:space:]]+true'
+minikube kubectl -- describe nodes | grep -E 'Unschedulable:[[:space:]]+true'
 
-if minikube kubectl -- get pods nginx-deployment |grep Pending;then
-    exit 0
-else
-    minikube kubectl -- delete deployment nginx-deployment
-    exit 1
-  fi
--- kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh
+++ kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh
@@ -4,3 +4,9 @@ cat ~/.zsh_history | grep kubectl |grep apply
 
 minikube kubectl -- describe deployment nginx-deployment
 
+# if minikube kubectl -- get pods nginx-deployment |grep Pending;then
+#     exit 0
+# else
+#     minikube kubectl -- delete deployment nginx-deployment
+#     exit 1
+#   fi
