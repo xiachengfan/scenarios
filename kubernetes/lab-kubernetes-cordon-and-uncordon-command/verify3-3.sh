49a2b94fd49f28c116709bacadbba92dc62a14bb
xiachengfan <xiachengfan@rockontrol.com>
2023-04-20 19:43:18 +0800

    modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh

--- kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh
+++ kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh
@@ -4,7 +4,7 @@ cat ~/.zsh_history | grep kubectl |grep apply
 
 POD=$(kubectl get pods -l app=nginx -o jsonpath='{.items[0].metadata.name}')
 
-if kubectl wait --for=condition=ready pod/$POD --timeout=2m; then
+if kubectl wait --for=condition=ready pod/$POD --timeout=3m; then
   exit 1
 else
   echo 0
