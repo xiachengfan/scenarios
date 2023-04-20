4a433dc0bcb84f929b665758d532c3bd102faee4
xiachengfan <xiachengfan@rockontrol.com>
2023-04-20 10:34:44 +0800

    new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/finish.md 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/index.json 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/intro.md 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/setup.sh 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/step1.md 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/step2.md 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/step3.md 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/step4.md 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/step5.md 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/verify1-1.sh 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/verify1-2.sh 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/verify1-3.sh 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/verify2-1.sh 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/verify3-1.sh 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/verify3-2.sh 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/verify4-1.sh 	new file:   kubernetes/lab-scaling-and-managing-pods-with-deployments/verify5.sh

---
+++ kubernetes/lab-scaling-and-managing-pods-with-deployments/verify3-2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IMAGE=nginx:1.19
+
+# 获取实际镜像版本
+IMAGE_CURRENT=$(kubectl get deployment my-deployment -o=jsonpath='{.spec.template.spec.containers[0].image}')
+
+# 比较镜像版本
+if [ "$IMAGE" = "$IMAGE_CURRENT" ]; then
+  exit 0
+else
+  exit 1
+fi
+
+cat ~/.zsh_history | grep kubectl |grep rollout |grep status
