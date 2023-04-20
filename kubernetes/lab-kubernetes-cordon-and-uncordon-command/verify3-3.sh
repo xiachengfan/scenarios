4cfd9b3a4124238fa99b4808d8ce2672bc64c534
xiachengfan <xiachengfan@rockontrol.com>
2023-04-20 22:08:24 +0800

    modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh

--- kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh
+++ kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 cat ~/.zsh_history | grep kubectl |grep apply
-minikube kubectl -- get pods | grep nginx 
-if $(minikube kubectl -- get pods | grep nginx | awk '{print $3}') -eq "Pending";then
-        exit 0
-esle
+if minikube kubectl -- describe nodes | grep -E 'Unschedulable:[[:space:]]+true';then
+exit 0
         minikube kubectl -- delete deployment nginx-deployment
         exit 1
 fi
+# minikube kubectl -- get pods | grep nginx 
+# if $(minikube kubectl -- get pods | grep nginx | awk '{print $3}') -eq "Pending";then
+#         exit 0
+# esle
+#         minikube kubectl -- delete deployment nginx-deployment
+#         exit 1
+# fi
