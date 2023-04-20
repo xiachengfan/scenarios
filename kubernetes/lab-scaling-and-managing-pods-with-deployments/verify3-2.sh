5b4123cfeee38fbe9fc88d9b31a28660f6d14e8b
xiachengfan <xiachengfan@rockontrol.com>
2023-04-20 20:34:15 +0800

    modified:   kubernetes/lab-scaling-and-managing-pods-with-deployments/verify3-2.sh

--- kubernetes/lab-scaling-and-managing-pods-with-deployments/verify3-2.sh
+++ kubernetes/lab-scaling-and-managing-pods-with-deployments/verify3-2.sh
@@ -3,9 +3,9 @@
 IMAGE=nginx:1.19
 
 # 获取实际镜像版本
-IMAGE_CURRENT=$(kubectl get deployment my-deployment -o=jsonpath='{.spec.template.spec.containers[0].image}')
+IMAGE_CURRENT=$(minikube kubectl -- get deployment my-deployment -o=jsonpath='{.spec.template.spec.containers[0].image}')
 
-if [ "$IMAGE" == "$IMAGE_CURRENT" ]; then
+if [ "$IMAGE" -eq "$IMAGE_CURRENT" ]; then
   exit 0
 else
   exit 1
