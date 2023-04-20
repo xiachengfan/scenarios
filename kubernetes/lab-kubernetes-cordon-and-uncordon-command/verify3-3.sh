c69675a308ec94148cd40f75c794d16e5bd4f137
xiachengfan <xiachengfan@rockontrol.com>
2023-04-20 20:15:09 +0800

    modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh 	modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-4.sh

--- kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh
+++ kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cat ~/.zsh_history | grep kubectl |grep apply
-sleep 60
 minikube kubectl -- get pods | grep nginx | grep "Pending"
--- kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-4.sh
+++ kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-4.sh
@@ -2,5 +2,8 @@
 
 cat ~/.zsh_history | grep kubectl |grep uncordon |grep minikube
 
-sleep 120
-minikube kubectl -- get pods | grep nginx |grep  Running
+if $(minikube kubectl -- get pods | grep nginx | awk '{print $3}') != "Pending";then
+        exit 0
+esle
+        exit 1
+fi
