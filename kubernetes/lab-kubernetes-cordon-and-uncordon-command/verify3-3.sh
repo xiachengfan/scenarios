47abdcb28fd2096a0615196688df98d51dce2477
xiachengfan <xiachengfan@rockontrol.com>
2023-04-20 20:06:27 +0800

    modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/step3.md 	modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh

--- kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh
+++ kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-3.sh
@@ -2,7 +2,7 @@
 
 cat ~/.zsh_history | grep kubectl |grep apply
 sleep 60
-if $(kubectl get pods | grep nginx | awk '{print $3}') == "Pending";then
+if $(minikube kubectl -- get pods | grep nginx | awk '{print $3}') == "Pending";then
         exit 0
 esle
         exit 2
