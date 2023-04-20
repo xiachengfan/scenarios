90c4a9525c9f4ccdca924b05295c3f2fd2262e5b
xiachengfan <xiachengfan@rockontrol.com>
2023-04-20 20:42:24 +0800

    modified:   kubernetes/lab-scaling-and-managing-pods-with-deployments/verify4-1.sh

--- kubernetes/lab-scaling-and-managing-pods-with-deployments/verify4-1.sh
+++ kubernetes/lab-scaling-and-managing-pods-with-deployments/verify4-1.sh
@@ -5,7 +5,7 @@ cat ~/.zsh_history | grep kubectl |grep rollout|grep undo
 IMAGE=nginx:latest
 
 # 获取实际镜像版本
-IMAGE_CURRENT=$(kubectl get deployment my-deployment -o=jsonpath='{.spec.template.spec.containers[0].image}')
+IMAGE_CURRENT=$(minikube kubectl -- get deployment my-deployment -o=jsonpath='{.spec.template.spec.containers[0].image}')
 
 # 比较镜像版本
 if [ "$IMAGE" = "$IMAGE_CURRENT" ]; then
