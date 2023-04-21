bcbf6e94f4bec62c09370e2324481997e06e26c4
xiachengfan <xiachengfan@rockontrol.com>
2023-04-21 14:55:30 +0800

    modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify4-2.sh 	modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify4-4.sh

--- kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify4-2.sh
+++ kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify4-2.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-
 cat ~/.zsh_history | grep kubectl |grep 'l' |grep cordon|grep org=labex 
-minikube kubectl -- describe node | grep SchedulingDisabled
--- kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify4-4.sh
+++ kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify4-4.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 cat ~/.zsh_history | grep kubectl |grep uncordon|grep org=labex
-if minikube kubectl -- describe node | grep SchedulingDisabled; then
-  exit 1
-else
-  echo 0
-fi
