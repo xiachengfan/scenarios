8c0945551659ac6d2e09b172bb3baea9d5a67619
xiachengfan <xiachengfan@rockontrol.com>
2023-04-23 19:01:50 +0800

    modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify2.sh 	modified:   kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-4.sh

--- kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify2.sh
+++ kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cat ~/.zsh_history | grep kubectl |grep uncordon |grep minikube
-minikube kubectl -- describe nodes | grep -E 'Unschedulable:[[:space:]]+false'
-
-cat /dev/null >  ~/.zsh_history
+if cat ~/.zsh_history | grep kubectl |grep uncordon |grep minikube > /dev/null&& minikube kubectl -- describe nodes | grep -E 'Unschedulable:[[:space:]]+false';then
+  history -c
+  exit 0
+else
+  exit 1
+  fi
--- kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-4.sh
+++ kubernetes/lab-kubernetes-cordon-and-uncordon-command/verify3-4.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cat ~/.zsh_history | grep kubectl |grep uncordon |grep minikube
 
-minikube kubectl -- get pods nginx |grep Running
 
-cat /dev/null >  ~/.zsh_history
+
+if cat ~/.zsh_history | grep kubectl |grep uncordon |grep minikube > /dev/null&& minikube kubectl -- get pods nginx |grep Running;then
+  history -c
+  exit 0
+else
+  exit 1
+  fi
