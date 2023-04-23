f9484e44114560e7cfc5aab56cbe686e38abee88
xiachengfan <xiachengfan@rockontrol.com>
2023-04-23 18:49:31 +0800

    modified:   kubernetes/lab-containerprobe-in-kubernetes/verify4-2.sh

--- kubernetes/lab-containerprobe-in-kubernetes/verify4-2.sh
+++ kubernetes/lab-containerprobe-in-kubernetes/verify4-2.sh
@@ -3,8 +3,8 @@
 cat ~/.zsh_history | grep kubectl |grep apply
 
 if minikube kubectl -- describe deployment containerprobe-deployment |grep -q "Readiness";then
-  exit 0
   history -c
+  exit 0
 else
   exit 1
   fi
