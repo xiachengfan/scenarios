27e8ec0e1e8d31fc2de5fca855e28bb46dece7fc
xiachengfan <xiachengfan@rockontrol.com>
2023-04-27 18:36:43 +0800

    modified:   kubernetes/lab-containerprobe-in-kubernetes/verify5-2.sh

--- kubernetes/lab-containerprobe-in-kubernetes/verify5-2.sh
+++ kubernetes/lab-containerprobe-in-kubernetes/verify5-2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pod_name=$(kubectl get pods -l app=containerprobe -o jsonpath='{.items[0].metadata.name}')
+pod_name=$(minikube kubectl -- get pods -l app=containerprobe -o jsonpath='{.items[0].metadata.name}')
 cat ~/.zsh_history | grep kubectl |grep describe|grep pod |grep ${pod_name}
